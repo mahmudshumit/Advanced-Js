5d83b72350b8c66349adc98bb54c21dd9cee09ea
mahmudshumit <rahmanshumit@gmail.com>
2021-02-02 20:42:22 +0600

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,18 @@
+// const numbers = [4,7,8,33,22];
+// const output =[];
+// for(let i = 0;i<numbers.length;i++){
+//     const element = numbers[i];
+//     const result = element*element;
+//     output.push(result);
+// }
+// console.log(output);
+numbers=[11,22,3,4,5,6]
+// const result = numbers.map(function(element){
+//     return element*element;
+     
+// })
+// const result = numbers.map(x => x *x );
+// console.log(result);
+
+const bigger = numbers.filter(x => x>5);
+console.log(bigger);
