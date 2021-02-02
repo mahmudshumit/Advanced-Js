c4b3baf7ec3001b72008c4be0c20d9560d4bf126
mahmudshumit <rahmanshumit@gmail.com>
2021-02-02 19:58:48 +0600

    true-false & null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+const result = add(9,9);
+console.log(result); 
+function add(num1,num2){
+    console.log(num1,num2);
+    
+}
+const result = add(9);
+console.log(result); 
+let student = {age:23,id:1444444};
+console.log(student.gf); 
+let student = undefined;
+console.log(student); 
---
+++ true-false.js
@@ -0,0 +1,27 @@
+//False
+        //0
+        //""
+        //undefined
+        //null
+        //NaN
+        //false
+
+
+//true
+        // " "
+        //"0"
+        //[]
+        //{}
+
+
+
+
+// const age = 0;
+const name =0;
+if(name ==0){
+    console.log("true");
+}
+else{
+    console.log("false");
+
+}
