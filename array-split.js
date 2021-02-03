1be3c43f6529d0b81a5d1db5ab4792a61802b39f
mahmudshumit <rahmanshumit@gmail.com>
2021-02-03 13:10:49 +0600

    array split

---
+++ array-split.js
@@ -0,0 +1,10 @@
+const nums =[1,2,3,4,5,6,7,8];
+const part = nums.slice(4,7);
+console.log(part);
+console.log(nums);
+
+const removed = nums.splice(2,3,88,99);
+console.log(removed);
+console.log(nums);
+const together = nums.join(" , ");
+console.log(together);
